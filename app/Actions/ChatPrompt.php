0f06ce3c38fb3e0aa75189eab6c17ed1d0e502fe
Mukesh Verma <mukeshverma643@gmail.com>
2023-11-13 12:55:28 +0530

    Configured OpenAI Api Key and Organization Key. Added Laravel Action

---
+++ app/Actions/ChatPrompt.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class ChatPrompt
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+}
